8ec2720b663137431ab1500c91a7f1d39cb446c3
rashmi <rashmi.kamath01@gmail.com>
2024-10-08 15:07:45 +0200

    testing commit name

---
+++ remove_duplicate.py
@@ -0,0 +1,13 @@
+from typing import List 
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[j] = nums[i]
+                j += 1
+        return j, nums
+            
+sol  = Solution()
+k , nums = sol.removeDuplicates([0,0,1,1,1,2,2,3,3,4])
+print(k, nums)
